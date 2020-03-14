1541af1334223fc071ccc4ea6236fc3af55a426b
Simon Hasak <simon.hasak@gmail.com>
2020-03-14 11:42:47 +0100

    Class for exchanging data in json format between microservices

--- src/main/java/sk/tuke/fei/hasak/notifyservice/kafka/NotifyServiceMessage.java
+++ src/main/java/sk/tuke/fei/hasak/notifyservice/kafka/NotifyServiceMessage.java
@@ -1,4 +1,18 @@
 package sk.tuke.fei.hasak.notifyservice.kafka;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class NotifyServiceMessage {
+
+    private String email;
+
+    private String message;
+
 }
