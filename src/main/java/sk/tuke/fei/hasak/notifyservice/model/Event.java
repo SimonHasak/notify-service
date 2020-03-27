9754476b58f0e93a8f07735acc67c018cc184ddd
Simon Hasak <simon.hasak@gmail.com>
2020-03-27 10:51:06 +0100

    Edited model for data persistence

--- src/main/java/sk/tuke/fei/hasak/notifyservice/model/Event.java
+++ src/main/java/sk/tuke/fei/hasak/notifyservice/model/Event.java
@@ -1,4 +1,19 @@
 package sk.tuke.fei.hasak.notifyservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.stereotype.Component;
+
+@Getter
+@ToString
+@Component
+@NoArgsConstructor
+@AllArgsConstructor
 public class Event {
+
+    private long id;
+    private String text;
+
 }
--- src/main/java/sk/tuke/fei/hasak/notifyservice/model/Notification.java
+++ src/main/java/sk/tuke/fei/hasak/notifyservice/model/Notification.java
@@ -1,4 +1,36 @@
 package sk.tuke.fei.hasak.notifyservice.model;
 
-public class Notification {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.hateoas.RepresentationModel;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+
+@Getter
+@Setter
+@Entity
+@ToString
+@NoArgsConstructor
+@Table(name = "notification_tb")
+public class Notification extends RepresentationModel<Notification> {
+
+    @Id
+    @Column(name = "contact_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long contactId;
+
+    @Column(name = "message_id", nullable = false)
+    private long messageId;
+
+    @Email
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    public Notification(long messageId, String email) {
+        this.messageId = messageId;
+        this.email = email;
+    }
 }
